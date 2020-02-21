301b2cf48b41f73c900adee6a627302a5390b314
srysdyk <srysdyk@iu.edu>
2020-02-21 14:12:19 -0500

    change decode location for python 3

--- iucas/utils.py
+++ iucas/utils.py
@@ -10,15 +10,21 @@ def validate_cas_ticket(casticket, casurl):
     """
     Takes a CAS Ticket and makes the out of bound GET request to
     cas.iu.edu to verify the ticket.
+
+    see: https://kb.iu.edu/d/bfpq#validate
     """
     validate_url = 'https://%s/cas/validate?cassvc=IU&casurl=%s' % \
         (settings.CAS_HOST, casurl,)
+    
     if hasattr(settings, 'CAS_HTTP_CERT'):
         h = httplib2.Http(ca_certs=settings.CAS_HTTP_CERT)
     else:
         h = httplib2.Http()
+    
     resp, content = h.request(validate_url,"GET")
-    return content.splitlines()
+    
+    # python3 requires this decode before split lines
+    return content.decode('utf-8').splitlines()
 
 
 def get_cas_username(casticket, casurl):
@@ -27,7 +33,7 @@ def get_cas_username(casticket, casurl):
     logged in user. If the user is not logged in returns None
     """
     resp = validate_cas_ticket(casticket, casurl)
-    if len(resp) == 2 and resp[0].decode('utf-8') == 'yes':
+    if len(resp) == 2 and resp[0] == 'yes':
         return resp[1]
     else:
         return None
@@ -38,15 +44,20 @@ class IUCASBackend(object):
     """
     def authenticate(self, ticket, casurl):
         resp = validate_cas_ticket(ticket, casurl)
-        if len(resp) == 2 and resp[0].decode('utf-8') == 'yes':
+        
+        if len(resp) == 2 and resp[0] == 'yes':
             username = resp[1]
+            
             if not username:
                 return None
+            
             try:
                 user = User.objects.get(username__iexact=username)
             except User.DoesNotExist:
                 return username
+            
             return user
+        
         else:
             return None
 
--- iucas/views.py
+++ iucas/views.py
@@ -4,8 +4,16 @@ from django.contrib import messages
 from django.conf import settings
 
 def iucas_validate(request):
+    # the login link on ktb/templates/ktb/index.html hits an IU 
+    # redirect that returns with the casticket as the param
+    # see https://kb.iu.edu/d/bfpq
+
+    # the authenicate method comes from this package's utils.py file
+    # however, it doesn't need to be imported because it is specified 
+    # in AUTHENTICATION_BACKENDS in opal.settings
     user = authenticate(ticket=request.GET["casticket"],casurl=request.build_absolute_uri())
     redirect_url = request.GET["next"]
+    
     if user is not None:
         if user.is_active:
             login(request, user)
@@ -15,4 +23,5 @@ def iucas_validate(request):
             pass
     else:
         messages.error(request, settings.CAS_NOT_REGISTERED_MSG)
+    
     return HttpResponseRedirect(redirect_url)
